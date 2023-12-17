1fe35aa5f1afccf96eb0c31da9e66ad9f455c986
Sydney Sisco <hello@sydsis.co>
2023-12-16 22:08:13 -0800

    basic matching is working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,85 +1,107 @@
-import { useState, useEffect, useContext } from 'react'
-import { ApiTest } from './components/ApiTest'
-import { socket } from './utils/socket'
-import { SocketTest } from './components/SocketTest'
-import { ConnectionState } from './components/ConnectionState'
+import React, { useState, useEffect } from 'react'
 import futureLogo from '/future.svg'
 import './App.css'
-
-import { AuthContext } from './AuthProvider';
-import { Route, Switch, Link, useLocation } from "wouter";
-import Login from './components/Login';
-import Register from './components/Register';
-import Gate from './components/Gate';
-import Logout from './components/Logout'
+import prompts from './prompts.json';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  const [isConnected, setIsConnected] = useState(socket.connected);
-
-  const { isLoggedIn, token, logout, user } = useContext(AuthContext);
+  const [currentPrompts, setCurrentPrompts] = useState([])
+  const [clickedImage, setClickedImage] = useState(null)
+  const [clickedPrompt, setClickedPrompt] = useState(null)
 
   useEffect(() => {
-    function onConnect() {
-      setIsConnected(true);
+    if (clickedImage === null || clickedPrompt === null) {
+      return
     }
 
-    function onDisconnect() {
-      setIsConnected(false);
+    if (clickedImage === clickedPrompt) {
+      alert('You win!')
+      setClickedImage(null)
+      setClickedPrompt(null)
+    } else {
+      alert('You lose!')
+      setClickedImage(null)
+      setClickedPrompt(null)
     }
+  }
+  , [clickedImage, clickedPrompt])
 
-    socket.on('connect', onConnect);
-    socket.on('disconnect', onDisconnect);
-
-    return () => {
-      socket.off('connect', onConnect);
-      socket.off('disconnect', onDisconnect);
-    };
-  }, []);
+  const getPrompts = () => {
+    // get 4 random prompts
+    const tempPrompts = []
+    for (let i = 0; i < 4; i++) {
+      const randomIndex = Math.floor(Math.random() * prompts.length)
+      tempPrompts.push(prompts[randomIndex])
+    }
+    setCurrentPrompts(tempPrompts)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://github.com/sydney-sisco/webapp-template" target="_blank">
-          <img src={futureLogo} className="logo" alt="future logo" />
-        </a>
-      </div>
-      <h1>webapp-template</h1>
-      <p className="read-the-docs">
-        Click on the logo to learn more
-      </p>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <ApiTest />
-      <SocketTest />
-      <ConnectionState isConnected={ isConnected } />
-
-      <Switch>
-          <Route path="/login"><Login /></Route>
-          <Route path="/register"><Register /></Route>
-
-          {!isLoggedIn ?
-            <Route>
-              <Gate />
-            </Route>
-            :
-            <>
-              <Route path="/auth">
-                You are logged in as {user.username}
-                <Logout />
-              </Route>
-            </>
-          }
-      </Switch>
+      <Columns items={currentPrompts}
+        setClickedImageIndex={setClickedImage}
+        setClickedPromptIndex={setClickedPrompt}
+      />
+      <button onClick={getPrompts}>Get Prompts</button>
     </>
   )
 }
 
 export default App
+
+const Columns = ({ items, setClickedImageIndex, setClickedPromptIndex }) => {
+
+  const handleImageClick = (itemIndex) => {
+    console.log('clicked', itemIndex)
+    setClickedImageIndex(itemIndex)
+  }
+
+  const handlePromptClick = (itemIndex) => {
+    console.log('clicked', itemIndex)
+    setClickedPromptIndex(itemIndex)
+  }
+
+
+  return(
+  <div className="container">
+    <div className="image-column">
+        <Images items={items} onClick={handleImageClick}/>
+    </div>
+    <div className="text-column">
+        <Prompts items={items} onClick={handlePromptClick}/>
+    </div>
+  </div>
+)};
+
+const Images = ({ items, onClick }) => {
+
+  const [randomizedItems, setRandomizedItems] = useState([])
+
+  useEffect(() => {
+    console.log('items', items);
+    const preSort = [...items];
+    const tempItems = preSort.sort(() => 0.5 - Math.random())
+    setRandomizedItems(tempItems)
+  }, [items])
+
+  return(
+    randomizedItems.map((item, index) => (
+      <img
+        key={index}
+        src={`https://magic-8-ball.sfo3.cdn.digitaloceanspaces.com/${item}`}
+        alt={item}
+        className="image-item"
+        onClick={() => onClick(item)}
+      />
+    ))
+  )
+}
+
+const Prompts = ({ items, onClick }) => (
+  items.map((item, index) => (
+    <span key={index} className="text-item"
+      onClick={() => onClick(item)}
+    >
+      {item.replaceAll('_', ' ').replace('.png', '')}
+    </span>
+  ))
+)
